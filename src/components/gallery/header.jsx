f1f6941626c989e7ea3ca79af4fecfa65d6b4665
sparkandflash <phegde04@gmail.com>
2025-05-21 01:14:57 +0530

    fix

--- src/components/gallery/header.jsx
+++ src/components/gallery/header.jsx
@@ -3,12 +3,11 @@ import { Box, Text } from '@chakra-ui/react'
 export default function Header() {
     return (
         <div> <Box
-            width="90%"
+            width="100%"
             h="30px"
             zIndex="1"
         >
             <Text>Varnexo Atryren Artworks</Text>
-         
         </Box>
         </div>
     )
